e9ef10d6c5e29175a95193d0582a0041a48f4c27
Nicolas Mayorga <nmayorgav@gmail.com>
2023-10-04 01:36:40 -0600

    documentation of the class based views

--- cores/views.py
+++ cores/views.py
@@ -5,10 +5,19 @@ from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework import permissions
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAdminUser, AllowAny
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg.openapi import Parameter, IN_HEADER, TYPE_STRING
+
+
+token_param = Parameter(
+    name="Authorization",
+    in_=IN_HEADER,
+    description="Token de autenticación en formato 'Bearer {token}'",
+    type=TYPE_STRING,
+    required=True
+)
 
 
 @api_view(['POST'])
@@ -25,40 +34,136 @@ def login(request):
 
 
 class ProductListView(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAdminUser]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @swagger_auto_schema(operation_description="Listado de productos y creación de un nuevo producto.",
+                        responses={200: ProductSerializer(many=True)})
+    def get(self, request, *args, **kwargs):
+        self.permission_classes = [AllowAny]
+        self.check_permissions(request)
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Crear un nuevo producto.",
+                        request_body=ProductSerializer,
+                        responses={201: ProductSerializer()},
+                        manual_parameters=[token_param])
+    def post(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
+        return super().post(request, *args, **kwargs)
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAdminUser]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @swagger_auto_schema(operation_description="Obtener detalles de un producto específico.",
+                        responses={200: ProductSerializer()})
     def get(self, request, *args, **kwargs):
+        self.permission_classes = [AllowAny]
+        self.check_permissions(request)
+        
         product = self.get_object()
         view, created = ProductView.objects.get_or_create(product=product)
         if not created:
             view.view_count += 1
             view.save()
-
         response = super().get(request, *args, **kwargs)
         return response
+
+    @swagger_auto_schema(operation_description="Actualizar un producto existente por completo.",
+                        request_body=ProductSerializer,
+                        responses={200: ProductSerializer()},
+                        manual_parameters=[token_param])
+    def put(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Actualizar parcialmente un producto existente.",
+                        request_body=ProductSerializer,
+                        responses={200: ProductSerializer()},
+                        manual_parameters=[token_param])
+    def patch(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Eliminar un producto.",
+                        responses={204: 'Producto eliminado correctamente.'},
+                        manual_parameters=[token_param])
+    def delete(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
+        return super().delete(request, *args, **kwargs)
+
 
 
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @swagger_auto_schema(operation_description="Listado de usuarios.",
+                        responses={200: UserSerializer(many=True)})
+    def get(self, request, *args, **kwargs):
+        self.permission_classes = [AllowAny]
+        self.check_permissions(request)
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Crear un nuevo usuario.",
+                        request_body=UserSerializer,
+                        responses={201: UserSerializer()},
+                        manual_parameters=[token_param])
+    def post(self, request, *args, **kwargs):
+        self.permission_classes = [AllowAny]
+        self.check_permissions(request)
+        return super().post(request, *args, **kwargs)
+
+
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @swagger_auto_schema(operation_description="Obtener detalles de un usuario específico.",
+                        responses={200: UserSerializer()},
+                        manual_parameters=[token_param])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Actualizar un usuario existente por completo.",
+                        request_body=UserSerializer,
+                        responses={200: UserSerializer()},
+                        manual_parameters=[token_param])
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Actualizar parcialmente un usuario existente.",
+                        request_body=UserSerializer,
+                        responses={200: UserSerializer()},
+                        manual_parameters=[token_param])
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Eliminar un usuario.",
+                        responses={204: 'Usuario eliminado correctamente.'},
+                        manual_parameters=[token_param])
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 
 class ProductAnalyticsView(generics.ListAPIView):
     queryset = ProductView.objects.all()
     serializer_class = ProductViewSerializer
+    permission_classes = [IsAdminUser]
+
+    @swagger_auto_schema(operation_description="Obtener estadísticas/análisis de productos ordenados por número de vistas.",
+                        responses={200: ProductViewSerializer(many=True)},
+                        manual_parameters=[token_param])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         return super().get_queryset().order_by('-view_count')
